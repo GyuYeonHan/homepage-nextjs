e1072625230334247ea81dfc9eeaf68b62508acf
gyuyeon <gks951020@gmail.com>
2022-03-04 18:25:43 +0900

    Layout.tsx 대문자로 수정

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import SideBar from "./SideBar";
 export default function Layout({ children }) {
   return (
     <div className="container w-full h-screen">
-      <div className="h-full flex justify-start items-center">
+      <div className="h-full flex justify-start items-center a">
         <SideBar />
         <div className="w-3/4">{children}</div>
       </div>

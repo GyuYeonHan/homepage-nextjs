bdaa648a8166cd60dec0275f7e6eaca206c2cb79
gyuyeon <gks951020@gmail.com>
2022-03-11 00:05:20 +0900

    base directory

--- apiCall/base.ts
+++ apiCall/base.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://http://ec2-3-39-57-119.ap-northeast-2.compute.amazonaws.com:8443";
+export const BASE_PATH = "https://ec2-3-39-57-119.ap-northeast-2.compute.amazonaws.com:8443";
 export const POST_PATH = "api/post";
 export const COMMENT_PATH = "api/comment";
 export const USER_PATH = "api/user";

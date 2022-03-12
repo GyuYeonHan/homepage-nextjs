b87635b60756f563a1080115d67af7ee6e7bb4a3
gyuyeon <gks951020@gmail.com>
2022-03-12 16:14:49 +0900

    유저 페이지

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Admin() {
   return (
     <div className="">
-      <span>관리자 페이지 입니다.</span>
+      <div>관리자 페이지 입니다.</div>
+      <Link href="/admin/user">
+        <a>유저 관리</a>
+      </Link>
     </div>
   );
 }
---
+++ pages/admin/user/index.tsx
@@ -0,0 +1,7 @@
+export default function User() {
+  return (
+    <div className="">
+      <div>유저 관리 페이지 입니다.</div>
+    </div>
+  );
+}

19e0380343efd60a7ee5ba52ba81a52d5a5a829b
gyuyeon <gks951020@gmail.com>
2022-03-12 23:07:19 +0900

    base path 변경

--- apiCall/base.ts
+++ apiCall/base.ts
@@ -1,5 +1,5 @@
-// export const BASE_PATH = "https://projects.yongcademy.kro.kr:8443";
-export const BASE_PATH = "http://localhost:8080";
+export const BASE_PATH = "https://projects.yongcademy.kro.kr:8443";
+// export const BASE_PATH = "http://localhost:8080";
 export const POST_PATH = "api/post";
 export const COMMENT_PATH = "api/comment";
 export const USER_PATH = "api/user";
